525a30ba560527ce145bc1674c60eed8c64eb6b1
sayobye <sayobye@mail.ru>
2017-10-15 19:51:07 +0300

    fix dijkstra-alg

--- src/bin/algorithms/Dijkstra-alg.js
+++ src/bin/algorithms/Dijkstra-alg.js
@@ -1,67 +1,81 @@
 const graph = {
   start: {
-    A: 6,
+    A: 5,
     B: 2,
   },
   A: {
-    end: 1, 
+    C: 4,
+    D: 2,
   },
   B: {
-    A: 2,
-    end: 5,
+    A: 8,
+    D: 7,
+  },
+  C: {
+    D: 6,
+    end: 3,
+  },
+  D: {
+    end: 1,
   },
   end: {},
 } 
 
 const costs = {
-  A: 6,
+  A: 5,
   B: 2,
+  C: Infinity,
+  D: Infinity,
   end: Infinity,
 } 
 
 const parents = {
   A: 'start',
   B: 'start',
+  C: null,
+  D: null,
   end: null,
 }
 
 const processed = []; 
-
+ 
 const findSmallestCostNode = (costs) => {
-  let smallestPath = Infinity;
-  let smallestCostNode = null;
+  let smallestPath = null;
+  let smallestCostNode = Infinity;
   for (node in costs) {
-    if (processed.indexOf(node) <= -1 && smallestPath > costs[node]) {
-      smallestPath = costs[node];
-      smallestCostNode = node;
+    if (processed.indexOf(node) == -1 && smallestCostNode > costs[node] && node !== 'end') {
+      smallestCostNode = costs[node];
+      smallestPath = node;
     }
   }
-  return smallestCostNode;
+  return smallestPath;
 }
-
+ 
 const alg = (graph) => {
-  while(Object.keys(graph).length - 1 !== processed.length) {
+  while (Object.keys(graph).length - 2 !== processed.length) {
     let node = findSmallestCostNode(costs);
     let neighbours = Object.keys(graph[node]);
     neighbours.forEach(nbr => {
-      if (costs[nbr] > costs[node] + graph[node][nbr]) { 
+      if (costs[nbr] > costs[node] + graph[node][nbr]) {
         costs[nbr] = costs[node] + graph[node][nbr];
         parents[nbr] = node;
-        processed.push(node);
-      }
+      } 
     });  
+    processed.push(node);
   }
 const findWay = parents => {
   let currentNode = parents.end;
   let way = 'end <- ';
-  const length = Object.keys(parents).length;
-  for (let i = 0; i < length; i++) {
+  while (currentNode !== 'start') {
     way += `${currentNode} <- `;
-    currentNode = parents[currentNode];
+    currentNode = parents[currentNode];  
   }
-  return way.slice(0,-3);
-} 
-  return `Кратчайший путь: ${costs.end} \nПуть: ${findWay(parents)}`
+  way += 'start';
+  return way;
+}
+  return `Cost: ${costs.end} \nPath: ${findWay(parents)}`
 }
 
 console.log(alg(graph));
+
+
